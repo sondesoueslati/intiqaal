05f8d9e5d09015ceef6c367e74ee7e0853d35dca
sondes <weslatisondes2@gmail.com>
2022-12-12 10:25:30 +0100

    initial commit

---
+++ send-mail.php
@@ -0,0 +1,37 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
+
+$to = "sales@intiqaal.com";
+$toName = "Intiqaal";
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+ 
+// echo 'iii',
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "	smtpout.secureserver.net";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 465;
+$mail->SMTPSecure = 'ssl';
+$mail->Username = "m.hadjsalem@intiqaal.com";
+$mail->Password = "Amiri%%12";
+
+$mail->setFrom($email, "Admin");
+$mail->AddReplyTo($email, $name);
+$mail->addAddress($to, $toName);
+
+$mail->Subject = $subject;
+$mail->Body = "Email: ".$email."\n Name: ".$name."\n Subject: ".$subject."\n Message: ".$message."\n";
+
+$mail->send();
+
+header("Location: index.html"); // Redirect after success 
